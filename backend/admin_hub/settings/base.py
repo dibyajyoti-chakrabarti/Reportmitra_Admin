4ac1ba8ccd35e7d34a79f689cc1f14c2b9c91483
Adidev <anandadidev43@gmail.com>
2026-01-09 14:18:05 +0530

    Prod ready code

--- backend/admin_hub/settings/base.py
+++ backend/admin_hub/settings/base.py
@@ -34,6 +34,7 @@ WSGI_APPLICATION = "admin_hub.wsgi.application"
 
 # Middleware
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -41,7 +42,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "admin_hub.urls"
